fcb3b47d08afca75cbb28d0835fee51831c9d766
Ali <aliabnar211@gmail.com>
2025-11-03 19:48:11 +0330

    feat: styles in navbar and home page, set CLI in package.json for run json-server

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,13 +4,13 @@ function Home() {
   return (
     <div>
       <Container>
-        <div className="shadow-xl grid-flow-col">
+        <div className="mt-4 flex w-full min-h-screen flex-col gap-4 p-4 rounded-lg bg-gray-100 shadow-xl">
           <div className="grid-cols-4">
             <h1>Collection</h1>
           </div>
-          <div className="grid-cols-8">
+          <div className="flex justify-end items-end w-full">
             <img
-              className="m-auto rounded-b-lg "
+              className="m-auto rounded-b-lg"
               src="https://www.zibamoon.com/Files/Thumbs/2021/3/3/%D8%A7%D8%B3%D8%AA%D8%A7%DB%8C%D9%84-%D9%BE%D8%B3%D8%B1%D8%A7%D9%86%D9%87-%D9%88-%D9%85%D8%B1%D8%AF%D8%A7%D9%86%D9%87-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%B9%DB%8C%D8%AF-1400.webp"
               alt=""
             />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,17 @@
+import Button from "../button/Button";
 import { Link } from "react-router-dom";
-import { FaCartShopping } from "react-icons/fa6";
 import { IoIosExit } from "react-icons/io";
 import Container from "../container/Container";
+import { FaCartShopping } from "react-icons/fa6";
 import { useShoppingCartContext } from "../context/ShoppingCartContext";
-import Button from "../button/Button";
+
 function Navbar() {
   const { cartQty, handleLogOut } = useShoppingCartContext();
 
   return (
-    <div className="h-14 border-b shadow ">
+    <div className="h-14 border-b bg-gray-100 shadow">
       <Container>
-        <div className=" flex justify-between flex-row items-center">
+        <div className="flex justify-between flex-row items-center">
           <ul className="flex flex-row">
             <li className="ml-4">
               <Link to="/">Home</Link>{" "}
@@ -20,14 +21,14 @@ function Navbar() {
             </li>
           </ul>
 
-          <div className="flex justify-center items-center pt-2">
+          <div className="mt-2 flex justify-center items-center gap-2 pt-2">
             <Button onClick={handleLogOut}>
-              <IoIosExit className="w-6 h-8 " />
+              <IoIosExit className="w-7 h-7 " />
             </Button>
             <Link className="relative" to="/cart">
               <button className="">
                 <FaCartShopping
-                  className="w-6 h-6
+                  className="w-7 h-7
                  flex items-center justify-center text-center"
                 />
               </button>
